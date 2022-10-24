17f296c044c486d51d6460c5c390a9511e33641b
Ayush <k.mishra.ayush98@gmail.com>
2022-10-24 12:56:47 +0530

    First Push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const changeHandler = (e) => setTask(e.target.value);
+  const clickHandler = (task) => {
+    const newTask = {
+      id: Math.random() * 100,
+      heading: task,
+      completed: false,
+    };
+    setTaskList((prev) => [...prev, newTask]);
+    setTask("");
+  };
+  const deleteTask = (id) => {
+    const newList = taskList.filter((item) => item.id !== id);
+    setTaskList(newList);
+  };
+  const editTask = (id) => {
+    setTaskList(
+      taskList.map((task) => {
+        if (task.id === id) {
+          console.log("completed");
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="input">
+        <input onChange={changeHandler} value={task} />
+        <button onClick={() => clickHandler(task)}>Submit</button>
+      </div>
+      <div className="list">
+        {taskList.map((task) => {
+          return (
+            <div
+              style={{
+                backgroundColor: task.completed ? "green" : "orange",
+              }}
+              className="task"
+              key={task.id}
+            >
+              <p>{task.heading}</p>
+              <div className="buttons">
+                <button className="delete" onClick={() => deleteTask(task.id)}>
+                  Delete
+                </button>
+                <button className="completed" onClick={() => editTask(task.id)}>
+                  Completed
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
 }
 
